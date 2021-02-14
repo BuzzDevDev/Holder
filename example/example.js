406f583b35573b8a06c9227345e754859768341c
ksplat <63369542+BuzzDevDev@users.noreply.github.com>
2021-02-14 14:43:13 -0500

    added some

--- example/example.js
+++ example/example.js
@@ -5,8 +5,20 @@ var options = {
     fileType: "json"
 };
 
-holder.getFile(options);
+var json = 
+{
+    "myData": ["foo", "bar"]
+};
+
+// returns an object with your apikey and passphrase
+// creates your json or other file with your data on it
+
+var myObj = holder.create(json);
 
 // Forgot your API key? Get your key with your passphrase.
+// Returns your api key
+var myKey = holder.getKey("00530055003100760054004700460074006100580070006c00510032003500560057006c0059007a0056006d0030007900540033004e003200530041003d003d"); 
 
-holder.getKey("Your passphrase"); // Returns your api key
+
+
+holder.reqFile(options);
--- holder.js
+++ holder.js
@@ -1,6 +1,15 @@
-const { createShort } = require("@ksplat/easytoken");
+/**
+ * @license MIT MIT License
+ * @author Bleart Emini
+ * 
+ */
+
+
+
+
 const EasyToken = require("@ksplat/easytoken");
-const fs  = require("fs")
+const { jsonReader } = require("./utils/jsonreader")
+const fs  = require("fs");
 const { String } = require("./utils/hash/hash");
 
 
@@ -10,30 +19,53 @@ var options = {
     length: 22
 };
 
-function createKey() {
+/**
+ * 
+ * @param {} file An object, string, number, or anything that needs to be written to a file.
+ * @param {String} fileType JSON or txt.
+ * @description Returns an object with your api key and passphrase. Creates a file with the data passed that can be requested with your api key.
+ */
+
+function create(file, fileType) {
     var apikey = EasyToken.createToken(options);
     var passphrase = EasyToken.createToken(options).hexEncode();
-    passphrase = Buffer.from(passphrase).toString('base64');
     var obj = {
         apikey: apikey,
         passphrase: passphrase
     };
-    console.log(obj);
+    return obj;
 };
 
-createKey();
+/**
+ * 
+ * @param {String} passphrase Your passphrase that you got by creating your key and passpharse.
+ * @description The gets your passpharse and returns a key from it. Used if you forgot your key.
+ */
 
 function getKey(passphrase) {
-    
+    var name = passphrase.decode();
+    jsonReader('./keys/keys.json', (err, data) => {
+        if(err) {
+            console.log('Error reading file:', err)
+            return;
+        };
+
+        if(data.hasOwnProperty(name)) {
+            return data[name].key;
+        };
+        
+        return console.log("Passphrase not found.");
+    });
 };
 
-function getFile(options) {
+function reqFile(options) {
     options = options || {
 
     };
 };
 
 module.exports = {
-    getFile: getFile,
-    getKey: getKey
+    reqFile: reqFile,
+    getKey: getKey,
+    create: create
 };
